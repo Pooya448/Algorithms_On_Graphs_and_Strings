c432d71b979fb273adc19b457cb3da17aa54196d
Pooya448 <pooya_kabiri@yahoo.com>
2019-02-28 14:25:01 +0330

    Complete

---
+++ A2/A2/Q1shortestPath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A2
+{
+    public class Q1ShortestPath : Processor
+    {
+        public Q1ShortestPath(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long,long[][], long, long, long>)Solve);
+        
+
+        public long Solve(long NodeCount, long[][] edges, long StartNode,  long EndNode)
+        {
+            Graph g = new Graph(NodeCount, edges);
+            return g.FindShortestPath((int)StartNode, (int)EndNode);
+            
+        }
+    }
+}
