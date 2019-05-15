f13125d93e5bfbfb71f519dce32a22df60e7e22c
Pooya448 <pooya_kabiri@yahoo.com>
2019-05-15 19:54:45 +0430

    Revert "Merge branch 'A8' of https://96521434.visualstudio.com/AD97982/_git/AD97982 into A8"
    This reverts commit 1a236ab00504e0e5c7f4713b0d829ada8b2ec819, reversing
    changes made to 53b6c541a171e4271c45c53302336c3f2bc8f887.

---
+++ A8/A8Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("A8Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A8Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1149b779-43c1-4437-a388-fdf7a5e09e39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
