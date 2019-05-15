5f9e385eac140c3141f9fd21069aeb7353fa878e
Pooya Kabiri <pooya_kabiri@yahoo.com>
2019-05-15 15:21:54 +0000

    Revert "Merged PR 93: A8"

--- Exam1/Exam1/Q2Cryptanalyst.cs
+++ Exam1/Exam1/Q2Cryptanalyst.cs
@@ -23,6 +23,8 @@ namespace Exam1
             bool f1 = false;
             bool f2 = false;
             bool f3 = false;
+            bool f4 = false;
+
             int i;
 
             int matchCounter = 0;
@@ -31,6 +33,10 @@ namespace Exam1
                 if (i == 100)
                 {
                     f1 = true;
+                }
+                if (i == 200)
+                {
+                    f4 = true;
                 }
                 if (i == 320)
                 {
