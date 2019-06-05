1e14bf4063eab0f28d603843ea3a3710b2f6aafb
Pooya448 <pooya_kabiri@yahoo.com>
2019-06-05 17:45:42 +0430

    Q1 Done

---
+++ A9/A9/EquationSolver.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A9
+{
+    class EquationSolver
+    {
+        public double[,] Matrix { get; set; }
+        public int MatrixSize { get; set; }
+        public EquationSolver(double[,] m,int n)
+        {
+            this.Matrix = m;
+            this.MatrixSize = n;
+        }
+        public double[] GaussianElimination()
+        {
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                var pivot = Matrix[i, i];
+                if (pivot == 0)
+                    for (int m = i + 1; m < MatrixSize; m++)
+                        if (Matrix[m,i] != 0)
+                            SwapRows(m, i);
+                MultiplyRow(i, 1 / pivot);
+                for (int j = i+1; j < MatrixSize; j++)
+                    if (Matrix[j , i] != 0)
+                        MakeZero(i, j);
+            }
+            return Round(BackSubstitution());
+        }
+
+        private double[] Round(double[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = Math.Round(2 * arr[i]) / 2;
+            return arr;
+        }
+
+        private double[] BackSubstitution()
+        {
+            double[] res = new double[MatrixSize];
+            res[MatrixSize - 1] = Matrix[MatrixSize - 1, MatrixSize];
+            for (int i = MatrixSize -2; i >= 0; i--)
+            {
+                res[i] = Matrix[i, MatrixSize];
+                for (int j = i + 1; j < MatrixSize; j++)
+                    res[i] = res[i] - Matrix[i, j] * res[j];
+                res[i] = res[i] / Matrix[i, i];
+            }
+            return res;
+        }
+
+        public void MakeZero(int i, int j)
+        {
+            double[] temps = new double[MatrixSize + 1];
+            for (int k = 0; k <= MatrixSize; k++)
+                temps[k] = Matrix[i, k] * Matrix[j, i];
+            for (int k = 0; k <= MatrixSize; k++)
+                Matrix[j, k] = Matrix[j,k] - temps[k];
+            return;
+        }
+        public void Substract(int n, int m)
+        {
+            for (int i = 0; i <= MatrixSize; i++)
+                Matrix[n, i] = Matrix[n, i] - Matrix[m, i];
+            return;
+        }
+        public void MultiplyRow(int n, double k)
+        {
+            for (int i = 0; i <= MatrixSize; i++)
+                Matrix[n, i] *= k;
+        }
+        public void SwapRows(int n, int m)
+        {
+            for (int i = 0; i <= MatrixSize; i++)
+                Swap(ref Matrix[n, i], ref Matrix[m, i]);
+            return;
+        }
+        public void Swap(ref double n, ref double m)
+        {
+            var temp = n;
+            n = m;
+            m = temp;
+            return;
+        }
+    }
+}
---
+++ A9/A9Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("A9Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A9Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("934acb02-8f37-4e3f-b0d9-b4dc9369abe5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
